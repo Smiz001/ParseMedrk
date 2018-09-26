f5f1f083f11a0145e67c9fb5e6e6e5dddc78f765
PosNik <kenismiz@gmail.com>
2018-09-26 08:43:01 +0300

    create element class

---
+++ ParseMedrk/Element.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseMedrk
+{
+  public class Element
+  {
+    public string NameElement { get; set; }
+    public string NameCategory { get; set; }
+    public string NameSubCategory { get; set; }
+    public string Price { get; set; }
+    public string Description { get; set; }
+  }
+}
